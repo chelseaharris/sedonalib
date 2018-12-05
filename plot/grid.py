f62a5f4ea7c37b46476b213fa67976c15f6b940a
Chelsea Harris <chelseaharris@berkeley.edu>
2018-12-04 21:11:22 -0500

    Replaced gplot.py with grid.py. Removed command line functionality. Untested.

---
+++ plot/grid.py
@@ -0,0 +1,64 @@
+#
+#
+# Authors: Dan Kasen, Chelsea Harris
+# Date: 2018-12-04
+# 
+# Purpose: Tools for plotting grid files
+#         
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+cLightSpeed = 3e10
+cAngPerCm = 1e8
+
+def plot_grid(grid_fn,
+              which_log='', zone_num=0, quantity='opacity', plot_nu=False, xlim=, ylim=, ls=, ):
+
+  quantity_labels = {'opacity'   :'opacity (cm^2/g)',
+                     'emissivity':'emissivity',
+                     'Jnu'       :'radiation field Jnu (ergs/s/Hz/cm^2/str)'}
+
+  # Load the grid
+  with h5py.File(grid_fn,'r') as data:
+    # path in the HDF5 file
+    zone = 'zonedata/{}'.format(zone_num)
+  
+    # Get frequency for x-axis
+    nu = np.array(data['nu'])
+  
+    # Get quantity to plot for y-axis
+    if quantity='Snu':
+      opac = np.array(data[zone+'/opacity'])
+      emis = np.array(data[zone+'/emissivity'])
+      vals = emis/opac
+    else:
+      vals = np.array(data[zone + '/' + type])
+  
+    # Plot
+    if plot_nu:
+      plt.plot(nu, vals, ls=ls)
+    else:
+      # Convert from frequency to wavelength if desired
+      lm = cLightSpeed*nu/cAngPerCm
+      plt.plot(lm, vals, ls=ls)
+    
+    if quantity in quantity_labels.keys():
+      ylabel = quantity_labels[quantity]
+    else:
+      ylabel = quantity
+
+    if 'x' in which_log:
+        plt.xscale('log')
+    if 'y' in which_log:
+        plt.yscale('log')
+
+    if len(xlim)==2:
+        plt.xlim(xlim)
+    
+    if len(ylim)==2:
+        plt.ylim(ylim)
+
+    plt.ion()
+    plt.show()
